902625e697ea4dc1de24dde7fdedc869f2f17517
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2018-12-01 12:58:33 +0100

    initial commit

---
+++ play/mongodb-find.js
@@ -0,0 +1,20 @@
+const {MongoClient, ObjectID} =require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDb server');       
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp')
+    
+    db.collection('Todos').find().toArray().then((docs) => {
+      console.log('Todos');
+      console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        if (err) {
+            console.log('Unable to insert todo', err);
+       }
+       console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+//    client.close();
+}); 
