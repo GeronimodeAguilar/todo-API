9a2ab798d9c721888b8b0578a77a9bd3333b6984
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2018-12-03 17:15:32 +0100

    add queries playgrond file

---
+++ play/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5c054099f96cd854a432157d';
+
+if (!ObjectID.isValid(id)) {
+  console.log('ID not valid');
+}
+
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('Todos', todos);
+});
+
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('Todo', todo);
+});
+
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return console.log('Id not found');
+  }
+  console.log('Todo By Id', todo);
+}).catch((e) => console.log(e));
+
+User.findById('5c03d3b0578ea038d0244247').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
