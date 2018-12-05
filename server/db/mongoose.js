ab0e8f6e25bbbede9750bf088c9a72064f671547
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2018-12-05 22:42:19 +0100

    debug switch mongoose.connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://GeronimodeAguilar:f3MmU654Xn9q2H2@ds159024.mlab.com:59024/node-todo-api', { useNewUrlParser: true });
+mongoose.connect('mongodb://GeronimodeAguilar:f3MmU654Xn9q2H2@ds159024.mlab.com:59024/node-todo-api' || process.env.MONGODB_URI , { useNewUrlParser: true });
 
 module.exports = {mongoose};
 
