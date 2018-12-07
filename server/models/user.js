11d83880e111698789d58f52f3096f80c78aca99
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2018-12-07 18:17:54 +0100

    add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -40,16 +40,16 @@ UserSchema.methods.toJSON = function () {
 };
 
 UserSchema.methods.generateAuthToken = function () {
-  let user = this;
-  let access = 'auth';
-  let token = jwt.sign({_id: user._id.toHexString(),access}, 'abc123').toString();
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
   user.tokens = user.tokens.concat([{access, token}]);
 
   return user.save().then(() => {
     return token;
-  })
-}
+  });
+};
 
 UserSchema.statics.findByToken = function (token) {
   var User = this;
