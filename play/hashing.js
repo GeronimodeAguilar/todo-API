aeec6beb098a18f188d1986dc9499353e9490bad
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2018-12-08 01:23:35 +0100

    hash user password before saving

--- play/hashing.js
+++ play/hashing.js
@@ -1,25 +1,41 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let message = 'I am the golden guy';
-let hash = SHA256(message).toString();
+let password = '123abc!';
 
-console.log(`Mesage: ${message}`);
-console.log(`Hash: ${hash}`);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash)=> {
+//         console.log(hash);
+//     });
+// });
 
-let data = {
-    id: 4
-};
-let token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-}
+ let hashedPassword = '$2a$10$uMUbGTAFqtKwNXio169eJudV07Tcfr8Ikk.DLss4wrPmXchuuVDcO';
+ bcrypt.compare(password, hashedPassword, (err,res) => {
+     console.log(res);
+ });
 
-//token.data.id = 5;
-//token.hash = SHA256(JSON.stringify(token.data)).toString();
 
-let resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-if (resultHash === token.hash) {
-    console.log('Data was not changed');
-} else {
-    console.log('Data was changed. Do not trust!');
-}
+// let message = 'I am the golden guy';
+// let hash = SHA256(message).toString();
+
+// console.log(`Mesage: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// let data = {
+//     id: 4
+// };
+// let token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+
+// //token.data.id = 5;
+// //token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// let resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed');
+// } else {
+//     console.log('Data was changed. Do not trust!');
+// }
